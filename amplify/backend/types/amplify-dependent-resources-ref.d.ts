3be66259d2ab053e328a0943c1baf96c2c7b4846
Naman Sancheti <naman.sancheti@gmail.com>
2023-01-08 15:27:56 -0800

    added support for adding image for a note

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s3e99dd265": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
